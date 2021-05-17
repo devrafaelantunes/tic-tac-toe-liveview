2eb4113eaaf25702b0ff51f0f99216d83a38475c
Rafael Antunes <78612579+devrafaelantunes@users.noreply.github.com>
2021-05-17 09:27:26 -0300

    refactoring

---
+++ lib/tic_tac_toe/game.ex
@@ -0,0 +1,52 @@
+defmodule TicTacToe.Game do
+
+  @winning_combinations [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  def fill_combinations(board) do
+    Enum.map(@winning_combinations, fn combination ->
+      Enum.map(combination, fn number ->
+        case Enum.at(board, number) do
+          "X" -> "X"
+          "O" -> "O"
+          _ -> number
+        end
+      end)
+    end)
+    |> winner?(board)
+  end
+
+  defp winner?(combinations, board) do
+    result = Enum.reduce(combinations, "", fn x, acc ->
+      case x do
+        ["X", "X", "X"] -> "X won" <> acc
+        ["O", "O", "O"] -> "O won" <> acc
+        _ -> acc
+      end
+    end)
+
+    if result == "" do
+      draw?(board)
+    else
+      result
+    end
+  end
+
+  defp draw?(board) do
+    string_list = List.to_string(board)
+
+    if String.length(string_list) == 9 do
+      "Draw"
+    else
+      ""
+    end
+  end
+end
--- lib/tic_tac_toe_web/live/game_live/game_live.ex
+++ lib/tic_tac_toe_web/live/game_live/game_live.ex
@@ -1,35 +1,15 @@
 defmodule TicTacToeWeb.GameLive do
   use TicTacToeWeb, :live_view
 
-  @combinations [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ]
+  alias TicTacToe.Game
 
   def mount(_params, _session, socket) do
     {:ok,
       socket
       |> assign(:turn, "X")
-      |> assign(:board, [
-        "", #0
-        "", #1
-        "", #2
-        "", #3
-        "", #4
-        "", #5
-        "", #6
-        "", #7
-        "", #8
-      ])
+      |> assign(:board, ["", "", "", "", "", "", "", "", ""])
       |> assign(:board_status, ["", "", "", "", "", "", "", "", ""])
       |> assign(:winner, "")}
-
   end
 
   def handle_event("clicked", %{"num" => num}, socket) do
@@ -44,48 +24,11 @@ defmodule TicTacToeWeb.GameLive do
     {:noreply,
       socket
       |> assign(:turn, turn)
-      |> assign(:board, List.replace_at(socket.assigns.board, String.to_integer(num), board))
-      |> assign(:board_status, List.replace_at(socket.assigns.board_status, String.to_integer(num), "disabled"))
-      |> assign(:winner, check_winner(List.replace_at(socket.assigns.board, String.to_integer(num), board)))}
+      |> assign(:board, change_board(socket, num, board))
+      |> assign(:board_status, change_board_status(socket, num))
+      |> assign(:winner, Game.fill_combinations(change_board(socket, num, board)))}
   end
 
-  def check_winner(board) do
-    Enum.map(@combinations, fn combination ->
-      Enum.map(combination, fn number ->
-        case Enum.at(board, number) do
-          "X" -> "X"
-          "O" -> "O"
-          _ -> number
-        end
-      end)
-    end)
-
-    |> verify_winner(board)
-  end
-
-  def verify_winner(combinations, board) do
-    result = Enum.reduce(combinations, "", fn x, acc ->
-      case x do
-        ["X", "X", "X"] -> "X won" <> acc
-        ["O", "O", "O"] -> "O won" <> acc
-        _ -> acc
-      end
-    end)
-
-    if result == "" do
-      verify_draw(board)
-    else
-      result
-    end
-  end
-
-  def verify_draw(board) do
-    string_list = List.to_string(board)
-
-    if String.length(string_list) == 9 do
-      "Draw"
-    else
-      ""
-    end
-  end
+  defp change_board(socket, num, param), do: List.replace_at(socket.assigns.board, String.to_integer(num), param)
+  defp change_board_status(socket, num), do: List.replace_at(socket.assigns.board_status, String.to_integer(num), "disabled")
 end
