5191038b8ecf15631080e21a38b945cb73d80c12
Rafael Antunes <78612579+devrafaelantunes@users.noreply.github.com>
2021-05-17 08:39:35 -0300

    first version completed

--- lib/tic_tac_toe_web/live/game_live/game_live.ex
+++ lib/tic_tac_toe_web/live/game_live/game_live.ex
@@ -1,6 +1,17 @@
 defmodule TicTacToeWeb.GameLive do
   use TicTacToeWeb, :live_view
 
+  @combinations [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
   def mount(_params, _session, socket) do
     {:ok,
       socket
@@ -16,41 +27,30 @@ defmodule TicTacToeWeb.GameLive do
         "", #7
         "", #8
       ])
+      |> assign(:board_status, ["", "", "", "", "", "", "", "", ""])
       |> assign(:winner, "")}
 
   end
 
   def handle_event("clicked", %{"num" => num}, socket) do
-    check_winner(socket.assigns.board)
-
     if socket.assigns.turn == "X" do
-      {:noreply,
-      socket
-      |> assign(:turn, "O")
-      |> assign(:board, List.replace_at(socket.assigns.board, String.to_integer(num), "X"))
-      |> assign(:winner, check_winner(socket.assigns.board))}
+      handle_reply(socket, num, "O", "X")
     else
-      {:noreply,
-      socket
-      |> assign(:turn, "X")
-      |> assign(:board, List.replace_at(socket.assigns.board, String.to_integer(num), "O"))
-      |> assign(:winner, check_winner(socket.assigns.board))}
+      handle_reply(socket, num, "X", "O")
     end
   end
 
-  def check_winner(board) do
-    combinations = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ]
+  def handle_reply(socket, num, turn, board) do
+    {:noreply,
+      socket
+      |> assign(:turn, turn)
+      |> assign(:board, List.replace_at(socket.assigns.board, String.to_integer(num), board))
+      |> assign(:board_status, List.replace_at(socket.assigns.board_status, String.to_integer(num), "disabled"))
+      |> assign(:winner, check_winner(List.replace_at(socket.assigns.board, String.to_integer(num), board)))}
+  end
 
-    Enum.map(combinations, fn combination ->
+  def check_winner(board) do
+    Enum.map(@combinations, fn combination ->
       Enum.map(combination, fn number ->
         case Enum.at(board, number) do
           "X" -> "X"
@@ -59,17 +59,33 @@ defmodule TicTacToeWeb.GameLive do
         end
       end)
     end)
-    |> verify_winner()
+
+    |> verify_winner(board)
   end
 
-  def verify_winner(board) do
-    Enum.reduce(board, "", fn combination, acc ->
-      IO.inspect(combination)
-      case combination do
-        ["X", "X", "X"] -> acc = "X won"
-        ["O", "O", "O"] -> acc = "O won"
-        _ -> acc = "Draw"
+  def verify_winner(combinations, board) do
+    result = Enum.reduce(combinations, "", fn x, acc ->
+      case x do
+        ["X", "X", "X"] -> "X won" <> acc
+        ["O", "O", "O"] -> "O won" <> acc
+        _ -> acc
       end
     end)
+
+    if result == "" do
+      verify_draw(board)
+    else
+      result
+    end
+  end
+
+  def verify_draw(board) do
+    string_list = List.to_string(board)
+
+    if String.length(string_list) == 9 do
+      "Draw"
+    else
+      ""
+    end
   end
 end
